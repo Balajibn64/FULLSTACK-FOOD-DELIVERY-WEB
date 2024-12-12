b1734bbf82059f688ffe34deb9080add3c567e31
Balajibn64 <balajibn6464@gmail.com>
2024-12-12 23:35:06 +0530

    change admin localhost

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -5,7 +5,7 @@ import {toast} from "react-toastify"
 
 const List = () => {
   
-  const url = "http://localhost:4000"
+  const url = "https://fullstack-food-delivery-web.onrender.com"
   const [list,setList] = useState([]);
 
   const fetchList = async () => {
