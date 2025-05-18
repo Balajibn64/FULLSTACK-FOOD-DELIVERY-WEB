0b9c08f465066ff61d5da591f159862571563148
Balaji N <147259911+Balajibn64@users.noreply.github.com>
2025-05-18 15:41:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://balajibn6464:9aIM1TkH81oHmO3j@cluster0.ic9hf.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://balajibn6464:GVYASziF8nZDuyLl@cluster0.ic9hf.mongodb.net/food-del').then(()=>console.log("DB Connected"));
 }
